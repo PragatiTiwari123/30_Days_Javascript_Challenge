0ba3e7cf28aec21f8d29a8b574e7c68eef61f2e6
PragatiTiwari123 <pragatip963@gmail.com>
2024-07-15 11:07:07 +0530

    task four complete

--- Day03.js
+++ Day03.js
@@ -41,3 +41,33 @@ else{
         console.log("Third number is the greatest number");
     }
 }
+
+// task 04
+
+let day_num = 3;
+
+switch (day_num) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        break;
+}
