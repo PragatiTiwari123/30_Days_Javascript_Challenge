c3ec87992b3273f37cb568476206d3859174e6c4
PragatiTiwari123 <pragatip963@gmail.com>
2024-07-16 18:51:13 +0530

    task three complete

--- Day04.js
+++ Day04.js
@@ -10,3 +10,14 @@ let num = 5;
 for(let i=1;i<=10;i++){
     console.log(num + " * " + i + " = "+(num*i));
 }
+
+// task 03
+
+let sum = 0, i = 1;
+
+while(i<=10){
+    sum += i;
+    i++;
+}
+console.log(`The sum of 10 numbers is ${sum}`);
+
