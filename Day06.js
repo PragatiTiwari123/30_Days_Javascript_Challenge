679e3ced2fcc1c521c6512cc4c8542c6eaa0cc13
PragatiTiwari123 <pragatip963@gmail.com>
2024-07-18 12:01:46 +0530

    task two complete

--- Day06.js
+++ Day06.js
@@ -5,3 +5,12 @@ arr.forEach(element => {
     console.log(element);
 });
 
+// task 02
+
+let arr1 = [10,20,30,40,50];
+for (let i = 0; i < arr1.length; i++) {
+    if(i===0 || i===arr1.length-1){
+        console.log(arr1[i]);
+    }
+    
+}
