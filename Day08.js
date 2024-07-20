aea1eb62d05158f8e7dc321fa1240b25d8a3d37c
PragatiTiwari123 <pragatip963@gmail.com>
2024-07-20 20:47:05 +0530

    task two complete

--- Day08.js
+++ Day08.js
@@ -2,3 +2,10 @@
 
 let name = "Hardik", age = 10;
 console.log(`Your name is ${name} \nYour age is ${age}`);
+
+// task 02
+
+let string = `Hello jii
+Welcome to chai aur code 
+Kaise hai ap log`;
+console.log(string);
